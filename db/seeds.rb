f33ff4e1cd6e16e51ecf40a6ce397be543a12249
cdwhitt <cwhitt91@gmail.com>
2020-09-18 15:57:59 -0400

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+require 'nokogiri'
+require 'open-uri'
+
+airlines = [
+	'Alaska Airlines',
+	'American Airlines',
+	'United Airlines',
+	'Delta Air Lines',
+	'Island Air Service',
+	'Spirit Airlines',
+	'JetBlue Airways',
+	'Cape Air',
+	'Allegiant Air LLC',
+	'Frontier Airlines',
+	'New Axis Airways',
+	'Boutique Air'
+]
+
+# scraping image urls
+doc = Nokogiri.HTML(URI.open('https://www.wego.com/airlines'))
+
+doc.css('airlines', 'div.airline lazy-image').each_with_index do |url, index|
+	Airline.create(name: airlines[index], image_url: url.attr('src'))
+end
+
+reviews =
+	Review.create(
+		[
+			{
+				title: 'Greate airline',
+				description: 'I really enjoyed my time flying with them',
+				score: 5,
+				airline: Airline.first
+			},
+			{
+				title: 'Loved it!',
+				description: 'Had a great time on the flight!',
+				score: 4,
+				airline: Airline.first
+			}
+		]
+	)
